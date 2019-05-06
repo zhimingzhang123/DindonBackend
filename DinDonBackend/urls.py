d39d707fd3e0f565fe043499b5816fc96dc971e5
giantzwlin <giantzwlin@gmail.com>
2019-05-06 23:43:23 +0800

    configured the urls

--- DinDonBackend/urls.py
+++ DinDonBackend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('dishes/', include('Dishes.urls')),
+    # path('announcements/', include('Announcements.urls')),
+    # path('orders/', include('Orders.urls')),
+    # path('tables/', include('Tables.urls')),
+    # path('users/', include('Users.urls'))
 ]
--- Dishes/views.py
+++ Dishes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from Dishes.models import Dish
+from Dishes.serializers import DishSerializer
+
+
+class DishListView(APIView):
+
+    def get(self, request):
+        dishes = Dish.objects.all()
+        serializer = DishSerializer(dishes, many=True)
+        return Response(serializer.data)
