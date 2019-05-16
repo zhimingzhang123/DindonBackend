8e56a412427a6725fad067500e142691abbde86d
zhangzhiming <173559542@qq.com>
2019-05-16 11:10:12 +0800

    添加服务员操作

--- DinDonBackend/permissions.py
+++ DinDonBackend/permissions.py
@@ -21,3 +21,7 @@ class CustomerPermission(permissions.BasePermission):
 class ChefPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.user_type == UserType.Chef
+
+class WaiterPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.user_type == UserType.Waiter
